a5029c1f4814270e03613b9b351eb7494023bc49
y3rbiadit0 <yerbiadito@gmail.com>
2025-03-18 10:28:41 +0100

    Add `camera_control_service.py` and MoveCamera service

---
+++ src/gesture_recognizer/scripts/camera_control_service.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import rospy
+from std_msgs.msg import Int8
+from gesture_recognizer.srv import MoveCamera, MoveCameraResponse
+import sys
+import tty
+import termios
+
+class CameraControlService:
+    def __init__(self):
+        rospy.init_node('camera_control_service', anonymous=True)
+        
+        # Publisher to send commands to the Raspberry Pi
+        self.pub = rospy.Publisher('/camera_move', Int8, queue_size=1)
+        
+        # Service to accept move commands
+        self.srv = rospy.Service('/move_camera', MoveCamera, self.handle_move_camera)
+        
+        self.running = True
+        rospy.on_shutdown(self.shutdown)
+        
+    def handle_move_camera(self, req):
+        command = req.command
+        print(command)
+        if command in [1, 2, 3]:  # Valid commands: 1=left, 2=right, 3=center
+            self.pub.publish(command)
+            return MoveCameraResponse(True, f"Sent command {command} to camera")
+        return MoveCameraResponse(False, "Invalid command (use 1, 2, or 3)")
+        
+    def getch(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+        
+    def keyboard_control(self):
+        print("Camera control - Use keys:")
+        print("'a': Left, 'd': Right, 's': Center, 'q': Quit")
+        print("Service available at '/move_camera'")
+        
+        while self.running and not rospy.is_shutdown():
+            char = self.getch()
+            
+            if char == 'a':  # Left
+                self.pub.publish(1)
+                print("Sent: Move left")
+            elif char == 'd':  # Right
+                self.pub.publish(2)
+                print("Sent: Move right")
+            elif char == 's':  # Center
+                self.pub.publish(3)
+                print("Sent: Move to center")
+            elif char == 'q':  # Quit
+                self.running = False
+                print("Shutting down")
+                
+    def shutdown(self):
+        self.running = False
+        self.pub.publish(3)  # Center camera on shutdown
+        
+    def run(self):
+        try:
+            self.keyboard_control()
+        except rospy.ROSInterruptException:
+            pass
+
+if __name__ == '__main__':
+    try:
+        controller = CameraControlService()
+        controller.run()
+        rospy.spin()
+    except Exception as e:
+        print(f"Error: {e}")
